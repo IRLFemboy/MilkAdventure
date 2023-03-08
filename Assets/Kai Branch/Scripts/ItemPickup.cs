9b81bd2331080132343cf7248a9383b06aefee7f
Primomac <118293160+Primomac@users.noreply.github.com>
2023-03-08 08:19:01 -0800

    Inventory System & Player
    - Inventory UI Canvas is in "Kai Test Scene"
    - PlayerController has no actual player
    - Items can be created from the Project Window: Right click > Create > Item

---
+++ Assets/Kai Branch/Scripts/ItemPickup.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    // Variables
+
+    public Item item;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerController>().pickup = gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerController>().pickup = null;
+        }
+    }
+}
